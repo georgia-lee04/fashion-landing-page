34659d0815ebf1fbab86dfa65ce76ae9a3f8eff3
georgia-lee04 <xgeorgialee@gmail.com>
2025-05-08 17:55:05 +0100

    Created image slider using swiper js

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
 const videos = document.querySelectorAll(".video");
 
 videos.forEach((video) => {
