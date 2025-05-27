66a8dd1d280ade633ecc5ec8eaa21ffdc25ebb59
georgia-lee04 <xgeorgialee@gmail.com>
2025-05-27 11:08:40 +0100

    Organised Women's products in new folder and updated links

--- product.js
+++ product.js
@@ -6,10 +6,10 @@ const products = [
     description:
       "Simple silhouettes remain timeless. The Rivi Dress is based on an archive style of ours – the Riviara Dress. The soft fabric is cut to a fitted shape with a flattering drape that fall across the body from the right hip.",
     images: [
-      "/images/allsaints-1.png",
-      "/images/allsaints-1.1.png",
-      "/images/allsaints-1.2.png",
-      "/images/allsaints-1-hover.png",
+      "/images/women/allsaints-1.png",
+      "/images/women/allsaints-1.1.png",
+      "/images/women/allsaints-1.2.png",
+      "/images/women/allsaints-1-hover.png",
     ],
   },
   {
@@ -19,10 +19,10 @@ const products = [
     description:
       "Where to start with The Louie Denim Shorts? These wide leg, clean cut denim shorts are made to a high rise silhouette. Match with chunky sandals and sunglasses for that AllSaints style spring outfit.",
     images: [
-      "/images/allsaints-2.png",
-      "/images/allsaints-2-hover.png",
-      "/images/allsaints-2.1.png",
-      "/images/allsaints-2.2.png",
+      "/images/women/allsaints-2.png",
+      "/images/women/allsaints-2-hover.png",
+      "/images/women/allsaints-2.1.png",
+      "/images/women/allsaints-2.2.png",
     ],
   },
   {
@@ -32,10 +32,10 @@ const products = [
     description:
       "Spring outfits and statement pieces go hand in hand which is why we adore our Flora Embroidered Top. With a gorgeous floral motif complimented by diamante straps, this versatile top can be worn over a t-shirt or on it's own - your choice. Flora by name, floral by nature.",
     images: [
-      "/images/allsaints-3.png",
-      "/images/allsaints-3-hover.png",
-      "/images/allsaints-3.1.png",
-      "/images/allsaints-3.2.png",
+      "/images/women/allsaints-3.png",
+      "/images/women/allsaints-3-hover.png",
+      "/images/women/allsaints-3.1.png",
+      "/images/women/allsaints-3.2.png",
     ],
   },
   {
@@ -45,10 +45,10 @@ const products = [
     description:
       "The Faye Shirt Dress is bold and stylish in equal measure. Crafted to an oversized silhouette, this piece is complimented by two chest pockets and a clean finish. With shacket button closure through the body, this shirt is putting an AllSaints spin on a classic style.",
     images: [
-      "/images/allsaints-4.png",
-      "/images/allsaints-4-hover.png",
-      "/images/allsaints-4.1.png",
-      "/images/allsaints-4.2.png",
+      "/images/women/allsaints-4.png",
+      "/images/women/allsaints-4-hover.png",
+      "/images/women/allsaints-4.1.png",
+      "/images/women/allsaints-4.2.png",
     ],
   },
   {
@@ -58,10 +58,10 @@ const products = [
     description:
       "The most perfect holiday dress. Our Rosa Hex dress is lightweight and breezy with a signature metal hex detail and trims inspired by AllSaints jewellery. The adjustable ties mean you can wear it your way.",
     images: [
-      "/images/allsaints-5.png",
-      "/images/allsaints-5-hover.png",
-      "/images/allsaints-5.1.png",
-      "/images/allsaints-5.2.png",
+      "/images/women/allsaints-5.png",
+      "/images/women/allsaints-5-hover.png",
+      "/images/women/allsaints-5.1.png",
+      "/images/women/allsaints-5.2.png",
     ],
   },
   {
@@ -71,10 +71,10 @@ const products = [
     description:
       "The Faye Dress is for laid-back dressing. It's got dual use – wear as a dress or a jacket. The oversized shape keeps things easy. Try it with chunky boots and trainers.",
     images: [
-      "/images/allsaints-6.png",
-      "/images/allsaints-6-hover.png",
-      "/images/allsaints-6.1.png",
-      "/images/allsaints-6.2.png",
+      "/images/women/allsaints-6.png",
+      "/images/women/allsaints-6-hover.png",
+      "/images/women/allsaints-6.1.png",
+      "/images/women/allsaints-6.2.png",
     ],
   },
   {
@@ -84,10 +84,10 @@ const products = [
     description:
       "Patch pockets on the front, style all over. The Leighton Jacket is an absolute winner for transitional dressing this spring. With eye catching features, wear it to stand out of the crowd at festivals or level up your outfit at the pub.",
     images: [
-      "/images/allsaints-7.png",
-      "/images/allsaints-7-hover.png",
-      "/images/allsaints-7.1.png",
-      "/images/allsaints-7.2.png",
+      "/images/women/allsaints-7.png",
+      "/images/women/allsaints-7-hover.png",
+      "/images/women/allsaints-7.1.png",
+      "/images/women/allsaints-7.2.png",
     ],
   },
   {
@@ -97,10 +97,10 @@ const products = [
     description:
       "Suit up in our Bonnie Trousers. They're shaped from a lightweight linen blend to a smart wide-leg silhouette and finished with a neat side zip. We love them with the matching bodice and blazer but the styling options are endless.",
     images: [
-      "/images/allsaints-8.png",
-      "/images/allsaints-8-hover.png",
-      "/images/allsaints-8.1.png",
-      "/images/allsaints-8.2.png",
+      "/images/women/allsaints-8.png",
+      "/images/women/allsaints-8-hover.png",
+      "/images/women/allsaints-8.1.png",
+      "/images/women/allsaints-8.2.png",
     ],
   },
   {
@@ -110,10 +110,10 @@ const products = [
     description:
       "The perfect skirt for spring doesn’t exist, but our Talia Maxi Skirt certainly comes close. Tiered? Yes. Voluminous? Yes. Lace trims? Absolutely, yes. Whether you’re strolling through the streets of Athens on your holiday or attending a wholesome wedding in the country - this statement skirt is sure to make an impression. Pair with the Talia Top to complete the look.",
     images: [
-      "/images/allsaints-9.png",
-      "/images/allsaints-9-hover.png",
-      "/images/allsaints-9.1.png",
-      "/images/allsaints-9.2.png",
+      "/images/women/allsaints-9.png",
+      "/images/women/allsaints-9-hover.png",
+      "/images/women/allsaints-9.1.png",
+      "/images/women/allsaints-9.2.png",
     ],
   },
   {
@@ -123,10 +123,10 @@ const products = [
     description:
       "The mini skirt – an icon woven into the fabric of British fashion history. Pamela is the AllSaints take. Offering both versatility and elegance, it's designed with shirred detailing and picot edge finishings. Ideal for everything from festivals to a night at the theatre, pair it with the Pamela Top for the complete look.",
     images: [
-      "/images/allsaints-10.png",
-      "/images/allsaints-10-hover.png",
-      "/images/allsaints-10.1.png",
-      "/images/allsaints-10.2.png",
+      "/images/women/allsaints-10.png",
+      "/images/women/allsaints-10-hover.png",
+      "/images/women/allsaints-10.1.png",
+      "/images/women/allsaints-10.2.png",
     ],
   },
 
@@ -137,10 +137,10 @@ const products = [
     description:
       "Add a spring to your step with The Asha Shoulder Bag. Skilfully woven from straw, the slouchy silhouette is finished with our signature branded leather patch at the front. The adjustable leather strap means it can be worn as a crossbody, too.",
     images: [
-      "/images/allsaints-11.png",
-      "/images/allsaints-11-hover.png",
-      "/images/allsaints-11.1.png",
-      "/images/allsaints-11.2.png",
+      "/images/women/allsaints-11.png",
+      "/images/women/allsaints-11-hover.png",
+      "/images/women/allsaints-11.1.png",
+      "/images/women/allsaints-11.2.png",
     ],
   },
   {
@@ -150,10 +150,10 @@ const products = [
     description:
       "A cool-girl summer staple with a twist. Our Louie Long Denim Shorts are imagined in metallic silver and shaped from cotton to a wide-leg silhouette. Let them stand out against a classic white tee and sandals combo.",
     images: [
-      "/images/allsaints-12.png",
-      "/images/allsaints-12-hover.png",
-      "/images/allsaints-12.1.png",
-      "/images/allsaints-12.2.png",
+      "/images/women/allsaints-12.png",
+      "/images/women/allsaints-12-hover.png",
+      "/images/women/allsaints-12.1.png",
+      "/images/women/allsaints-12.2.png",
     ],
   },
   {
@@ -163,10 +163,10 @@ const products = [
     description:
       "Go for it in the Charli Trousers. The cut-out embroidered fabric has a loose straight shape with an elastic waistband to add shape and comfort. Complete with pintuck detail down the front and a playful scallop hem. They offer a feminine, but fun option for effortless summer dressing - just add the matching shirt for the full look.",
     images: [
-      "/images/allsaints-13.png",
-      "/images/allsaints-13-hover.png",
-      "/images/allsaints-13.1.png",
-      "/images/allsaints-13.2.png",
+      "/images/women/allsaints-13.png",
+      "/images/women/allsaints-13-hover.png",
+      "/images/women/allsaints-13.1.png",
+      "/images/women/allsaints-13.2.png",
     ],
   },
   {
@@ -176,10 +176,10 @@ const products = [
     description:
       "Looking for something easy to wear but pretty? You're looking for Delphine. Cut from a luxe silk and linen blend to a maxi silhouette, it features a paisley print designed for this dress. Lace trims and elasticated, ruffle cuffs create feminine drama.",
     images: [
-      "/images/allsaints-14.png",
-      "/images/allsaints-14-hover.png",
-      "/images/allsaints-14.1.png",
-      "/images/allsaints-14.2.png",
+      "/images/women/allsaints-14.png",
+      "/images/women/allsaints-14-hover.png",
+      "/images/women/allsaints-14.1.png",
+      "/images/women/allsaints-14.2.png",
     ],
   },
 ];
